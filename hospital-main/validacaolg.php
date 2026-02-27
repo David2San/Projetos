4c2c20b7a14790de41024b92096d30b6d06e8afb
David Reis <111019311+David2San@users.noreply.github.com>
2026-02-26 21:46:35 -0300

    Update validacaolg.php

--- hospital-main/validacaolg.php
+++ hospital-main/validacaolg.php
@@ -1,2 +1,28 @@
+<?php
+
+// Responsavel por criar uma conexão com meu banco
+
+function abrirBanco () {
+    $conexao = new mysqli("localhost", "root", "root", "agenda");
+
+    return $conexao;
+}
+
+// Função Responsavel por inserir uma pessoa no meu banco de dados
+
+function inserirPessoa($id_login, $id_usuario, $usuario, $senha) {
+    $banco = abrirBanco();
+    $sql = "INSERT INTO pessoa($id_login, $id_usuario, $usuario, $senha)
+    VALUES ('{$id_login}','{$id_usuario}','{$usuario}','{$senha}')";
+    $banco->query($sql);
+    $banco->close();
+    voltarIndex();
+}
+
+// Após Inserir Uma nova pessoa, retorna a pagina principal
+
+function voltarIndex() {
+    header("Location:principal.php?msg=Cadastro");
+}
 
 ?>
